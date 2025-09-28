0be9746bd6063da092ca02be0901d66040a9648f
Felipe Coletti <83978518+felipe-coletti@users.noreply.github.com>
2025-09-28 20:38:07 +0000

    feat: enhance system info command with detailed output

--- cmd/system/info.go
+++ cmd/system/info.go
@@ -4,14 +4,57 @@ import (
 	"cli/internal/cli"
 	"cli/internal/meta"
 	"fmt"
+	"os"
 	"runtime"
+	"text/tabwriter"
+	"time"
 )
 
 func InfoRun(args []string) {
-	fmt.Println("=== System Info ===")
-	fmt.Printf("OS: %s\n", runtime.GOOS)
-	fmt.Printf("Architecture: %s\n", runtime.GOARCH)
-	fmt.Printf("CPU Cores: %d\n", runtime.NumCPU())
+	hostname, _ := os.Hostname()
+	now := time.Now()
+
+	dateFormat := "2006-01-02"
+	timeFormat := "15:04:05"
+	zoneFormat := "MSTZ07:00"
+
+	currentDate := now.Format(dateFormat)
+	currentTime := now.Format(timeFormat)
+	currentZone := now.Format(zoneFormat)
+
+	user := os.Getenv("USER")
+
+	if user == "" {
+		user = os.Getenv("USERNAME")
+	}
+
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+
+	memUsedMB := float64(m.Alloc) / 1024 / 1024
+
+	workingDir, _ := os.Getwd()
+
+	writer := tabwriter.NewWriter(os.Stdout, 0, 8, 2, '\t', 0)
+	defer writer.Flush()
+
+	fmt.Fprintln(writer, "=== System Info ===")
+	fmt.Fprintf(writer, "OS:\t%s\n", runtime.GOOS)
+	fmt.Fprintf(writer, "Architecture:\t%s\n", runtime.GOARCH)
+	fmt.Fprintf(writer, "CPU Cores:\t%d\n", runtime.NumCPU())
+	fmt.Fprintf(writer, "Environment Variables:\t%d\n\n", len(os.Environ()))
+	fmt.Fprintln(writer, "=== Host Info ===")
+	fmt.Fprintf(writer, "Hostname:\t%s\n", hostname)
+	fmt.Fprintf(writer, "User:\t%s\n", user)
+	fmt.Fprintf(writer, "Date:\t%s\n", currentDate)
+	fmt.Fprintf(writer, "Time:\t%s\n", currentTime)
+	fmt.Fprintf(writer, "Time Zone:\t%s\n\n", currentZone)
+	fmt.Fprintln(writer, "=== Process Info ===")
+	fmt.Fprintf(writer, "PID:\t%d\n", os.Getpid())
+	fmt.Fprintf(writer, "Parent PID:\t%d\n", os.Getppid())
+	fmt.Fprintf(writer, "Working Dir:\t%s\n", workingDir)
+	fmt.Fprintf(writer, "Memory Used:\t%.2f MB\n", memUsedMB)
+
 }
 
 func InfoHelp() {
